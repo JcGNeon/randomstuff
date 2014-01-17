47f6a28344de2a3de27f844ec6dcd66edf5902b6
JcGNeon <hisame129375860@hotmail.com>
2014-01-17 02:15:57 -0500

    Downloads images from websites.

---
+++ crawler.py
@@ -0,0 +1,39 @@
+__author__ = 'neon'
+from bs4 import BeautifulSoup
+from sys import exit
+import urllib
+import urllib2
+import re
+import os
+
+def openUrl():
+    try:
+        result = urllib2.urlopen('http://reddit.com/', data = None)
+        soup = BeautifulSoup(result)
+        getUrl(soup)
+    except Exception, e:
+        print e
+        exit(1)
+
+def getUrl(soup):
+    for link in soup.find_all('a'):
+        new_result = link.get('href')
+        someMatch = re.search(r'.jpg|png', str(new_result))
+        if(someMatch != None):
+            print new_result
+            url = str(new_result).split('/')[-1]
+            try:
+                if os.path.isfile(url):
+                    pass
+                else:
+                    urllib.urlretrieve(new_result, filename = url, reporthook = None, data = None)
+            except Exception, e:
+                print e
+
+def main():
+    openUrl()
+    print 'Done.'
+    exit(0)
+
+if __name__ == '__main__':
+    main()
